a295e87abafd9c03515d77e47176f1b33b0a211e
David J Birnbaum <djbpitt@gmail.com>
2023-02-07 16:22:34 -0500

    Add pipeline to print individual nodes

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -4,7 +4,8 @@ import scalax.collection.GraphPredef._
 import scalax.collection.GraphEdge._
 @main def hello(): Unit =
   println("Hello, Scala 3!")
-  val h = mutable.Graph.empty[Int, UnDiEdge]
-  h += 0
-  h += 1
+  val h = mutable.Graph.empty[Int, DiEdge]
+  h += (1 ~> 0) // add directed edge, implicitly adds nodes
   println(h.nodes)
+  println(h.edges)
+  h.nodes.foreach(println) // pipeline
