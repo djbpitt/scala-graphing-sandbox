e58f4e662831a004db0d57245852f66b12bd409c
David J Birnbaum <djbpitt@gmail.com>
2023-02-07 16:53:49 -0500

    Add nodes and directed edge
    Give node a name before adding to avoid having to search for it

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -3,10 +3,15 @@ import scalax.collection.mutable
 import scalax.collection.GraphPredef._
 import scalax.collection.GraphEdge._
 case class A_node(id: Int, tokens:List[String])
-case class E()
+case class A_edge_labels (witnesses: List[String])
+
 @main def hello(): Unit =
   val h = mutable.Graph.empty[A_node, DiEdge]
-  h += A_node(0, List("a", "b"))
+  val n0 = A_node(0, List("a", "b"))
+  h += n0
+  val n1 = A_node(1, List("c", "d"))
+  h += n1
+  h += (n0 ~> n1)
   println(h.nodes)
   println(h.edges)
   h.nodes.foreach(println) // pipeline
