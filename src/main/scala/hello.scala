223cf60f141b6e4eee0d49bd3beb3027045b77a3
David J Birnbaum <djbpitt@gmail.com>
2023-02-07 16:33:01 -0500

    Use case class to add properties to nodes

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -2,10 +2,11 @@ import scalax.collection.Graph
 import scalax.collection.mutable
 import scalax.collection.GraphPredef._
 import scalax.collection.GraphEdge._
+case class N(id: Int, tokens:List[String])
 @main def hello(): Unit =
-  println("Hello, Scala 3!")
-  val h = mutable.Graph.empty[Int, DiEdge]
-  h += (1 ~> 0) // add directed edge, implicitly adds nodes
+  val h = mutable.Graph.empty[N, DiEdge]
+  val n0 = N(0, List("a", "b"))
+  h += n0
   println(h.nodes)
   println(h.edges)
   h.nodes.foreach(println) // pipeline
