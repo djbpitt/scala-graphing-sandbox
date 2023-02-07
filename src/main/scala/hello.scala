ad89596b24e833dbd51a27410378d1b99bca3267
David J Birnbaum <djbpitt@gmail.com>
2023-02-07 16:39:11 -0500

    Use safer name for class

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -2,11 +2,11 @@ import scalax.collection.Graph
 import scalax.collection.mutable
 import scalax.collection.GraphPredef._
 import scalax.collection.GraphEdge._
-case class N(id: Int, tokens:List[String])
+case class A_node(id: Int, tokens:List[String])
+case class E()
 @main def hello(): Unit =
-  val h = mutable.Graph.empty[N, DiEdge]
-  val n0 = N(0, List("a", "b"))
-  h += n0
+  val h = mutable.Graph.empty[A_node, DiEdge]
+  h += A_node(0, List("a", "b"))
   println(h.nodes)
   println(h.edges)
   h.nodes.foreach(println) // pipeline
