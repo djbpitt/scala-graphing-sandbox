94d0510f70dc32774fa2fb1d26df7fb8091fc80b
David J Birnbaum <djbpitt@gmail.com>
2023-02-07 16:18:33 -0500

    Add nodes to mutable graph

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -1,3 +1,10 @@
+import scalax.collection.Graph
+import scalax.collection.mutable
+import scalax.collection.GraphPredef._
+import scalax.collection.GraphEdge._
 @main def hello(): Unit =
   println("Hello, Scala 3!")
-
+  val h = mutable.Graph.empty[Int, UnDiEdge]
+  h += 0
+  h += 1
+  println(h.nodes)
