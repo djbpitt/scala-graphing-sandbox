73ab50778bae08ef9b9bfe970a67072f3c4c268b
rhdekker <ronald.dekker@di.huc.knaw.nl>
2023-02-08 18:41:49 +0100

    Extract the readings from the JSON. Using a case class would be better.

--- src/main/scala/unaligned_dev.scala
+++ src/main/scala/unaligned_dev.scala
@@ -1,4 +1,4 @@
-import play.api.libs.json.Json
+import play.api.libs.json.{JsArray, JsObject, Json}
 
 @main def unaligned_dev():Unit =
   val wd = os.pwd
@@ -9,23 +9,17 @@ import play.api.libs.json.Json
   val fileContents = os.read(datafile_path)
   // println(fileContents)
   val json = Json.parse(fileContents)
-  println(json)
+  // println(json)
+
+  json match
+    case JsArray(arr) =>
+      for node <- arr do
+        node match
+          case JsObject(obj) =>
+            val readings = obj("readings")
+            println(readings)
 
 
-
-//
-//  val result = JSON.parseFull(jsonStr)
-//  result match {
-//
-//    // Matches if jsonStr is valid JSON and represents a Map of Strings to Any
-//    case Some(map: Map[String, Any]) => println(map)
-//    case None => println("Parsing failed")
-//    case other => println("Unknown data structure: " + other)
-//  }
-
-//with open("unaligned_data.json", "r") as f:
-//  darwin = json.load(f)
-//
 //  for node in darwin: # Each unaligned zone is its own node
 //    readings = node["readings"] # list of lists
 //    current_linkage_object, current_cophenetic = create_linkage_object(readings)
