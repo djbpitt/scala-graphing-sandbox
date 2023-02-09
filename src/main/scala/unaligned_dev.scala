4fafcd54a6dd3626808b32f3dc42fe1f0244ddf6
rhdekker <ronald.dekker@di.huc.knaw.nl>
2023-02-09 19:48:12 +0100

    Extracted vectorize_unaligned_fragment method. Removed curly braces in UnaliignedFragment object. Use left to right computational pipeline construct in main method.

--- src/main/scala/unaligned_dev.scala
+++ src/main/scala/unaligned_dev.scala
@@ -7,12 +7,11 @@ case class UnalignedFragment(nodeno: Int, readings: List[List[String]])
 
 // object is used, but somehow IDE doesn't detect it's usage.
 @unused
-object UnalignedFragment {
+object UnalignedFragment:
   implicit val reads: Reads[UnalignedFragment] = (
     (__ \ "nodeno").read[Int] and
       (__ \ "readings").read[List[List[String]]]
     )(UnalignedFragment.apply _)
-}
 
 def read_data: List[UnalignedFragment] = {
   val wd = os.pwd
@@ -27,25 +26,26 @@ def read_data: List[UnalignedFragment] = {
   //  println(darwin)
   darwin
 }
+def vectorize_unaligned_fragment(node: UnalignedFragment): Unit = {
+  // we have got to convert each reading into a bag of words
+  // that is a map/transform operation
+  // have to join tokens into a string again for this library to work
+  // Then we vectorize each reading using the bag of words of that reading
+  // and the terms of the combined keys of all the bags of words
+
+  val list_bags_of_readings_joined = node.readings.map(reading => pimpString(reading.mkString(" ")).bag(filter = "", stemmer = None))
+  println(list_bags_of_readings_joined)
+
+  // calculate combined keys of bags.
+  val keys = list_bags_of_readings_joined.map(bag => bag.keySet)
+  println(keys)
+  val terms = keys.reduce(_.union(_))
+  println(terms)
+}
 @main def unaligned_dev():Unit =
   val darwin: List[UnalignedFragment] = read_data
 
-  for node <- darwin
-    if node.nodeno == 1146 do
-      // we have got to convert each reading into a bag of words
-      // that is a map/transform operation
-      // have to join tokens into a string again for this library to work
-      // Then we vectorize each reading using the bag of words of that reading
-      // and the terms of the combined keys of all the bags of words
-
-      val list_bags_of_readings_joined = node.readings.map(reading => pimpString(reading.mkString(" ")).bag(filter="", stemmer=None))
-      println(list_bags_of_readings_joined)
-
-      // calculate combined keys of bags.
-      val keys = list_bags_of_readings_joined.map( bag => bag.keySet)
-      println(keys)
-      val terms = keys.reduce(_.union(_))
-      println(terms)
+  darwin.filter(_.nodeno == 1146).foreach(vectorize_unaligned_fragment)
 
 
 
