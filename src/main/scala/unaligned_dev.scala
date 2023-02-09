cb81a5dfc378b2b348be1cf92893fcb6764a3381
David J Birnbaum <djbpitt@gmail.com>
2023-02-09 17:16:31 -0500

    Render dendrogram despite deprecation of show() (which calls a macro)
    Cluster readings
    Map to dendrogram
    Use desktop() instead of show()

--- src/main/scala/unaligned_dev.scala
+++ src/main/scala/unaligned_dev.scala
@@ -1,20 +1,22 @@
 import play.api.libs.functional.syntax.toFunctionalBuilderOps
 import play.api.libs.json.{JsArray, JsObject, Json, Reads, __}
-
 import scala.annotation.unused // tell IDE not to raise error for indirect reference
 import smile.nlp.pimpString // extended string with additional properties (we use .bag())
+import smile.clustering.{hclust, HierarchicalClustering} // https://haifengl.github.io/clustering.html#hierarchical
+import smile.plot.show
+import smile.plot.swing._
+import smile.plot.Render._
 case class UnalignedFragment(nodeno: Int, readings: List[List[String]])
 
-// object is used, but somehow IDE doesn't detect its usage.
-@unused
+@unused // object is used, but somehow IDE doesn't detect its usage.
 object UnalignedFragment: // add method to read from JSON into case class
   // implicit means lazy evaluation (maybe?); Reads is part of play library
-  implicit val reads: Reads[UnalignedFragment] = ( // Read into tuple, which is passed to constructor
+  implicit val reads: Reads[UnalignedFragment] = (// Read into tuple, which is passed to constructor
     (__ \ "nodeno").read[Int] and // __ is part of play; name of property to read; values are assigned by position
       (__ \ "readings").read[List[List[String]]]
     )(UnalignedFragment.apply _) // call UnalignedFragment constructor with our properties
 
-def read_data: List[UnalignedFragment] = {
+def read_data: List[UnalignedFragment] =
   val wd = os.pwd
   println(wd)
 
@@ -26,14 +28,13 @@ def read_data: List[UnalignedFragment] = {
   val darwin = parsed.get
   //  println(darwin)
   darwin
-}
-def vectorize_unaligned_fragment(node: UnalignedFragment): Unit = { // Unit is the type for a void function
+
+def vectorize_unaligned_fragment(node: UnalignedFragment): List[Array[Double]] = // Unit is the type for a void function
   // we have got to convert each reading into a bag of words
   // that is a map/transform operation
   // have to join tokens into a string again for this library to work
   // Then we vectorize each reading using the bag of words of that reading
   // and the terms of the combined keys of all the bags of words
-
   // By default filter removes stopwords and stemmer stems, so turn those off
   // NB: filter has to be set to empty string, and not to None, for no obvious reason
   val list_bags_of_readings = node.readings.map(reading => pimpString(reading.mkString(" ")).bag(filter = "", stemmer = None))
@@ -49,12 +50,20 @@ def vectorize_unaligned_fragment(node: UnalignedFragment): Unit = { // Unit is t
   // vectors.foreach(println(_.getClass())) // doesn't work; needs type info
   vectors.foreach(e => println(e.mkString("Array(", ", ", ")")))
   // println(vectors.head.mkString("Array(", ", ", ")"))
-}
-@main def unaligned_dev():Unit =
+  vectors
+
+def cluster_readings(data: List[Array[Double]]): HierarchicalClustering =
+  hclust(data.toArray, "ward")
+
+@main def unaligned_dev(): Unit =
   val darwin: List[UnalignedFragment] = read_data
   // we know there's only one, so we could have told it to find the first
   //   and then skipped the foreach()
-  darwin.filter(_.nodeno == 1146).foreach(vectorize_unaligned_fragment)
+  darwin.filter(_.nodeno == 1146).
+    map(vectorize_unaligned_fragment).
+    map(cluster_readings).
+    map(dendrogram)
+    .foreach(e => desktop(e))
 
 
 
