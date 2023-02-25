278f098bf8724d3b2079dc93feaa81f0c4ee9be4
David J Birnbaum <djbpitt@gmail.com>
2023-02-25 16:23:44 -0500

    Move create_token_array out of main
    Specify path to data inside main
    Parameterize read_data

--- src/main/scala/reptilian/reptilian.scala
+++ src/main/scala/reptilian/reptilian.scala
@@ -1,22 +1,27 @@
 package reptilian
 
+import os.Path
 
-/** Read hard-coded path to data filenames (one file per witness)
+/** Read data files from supplied path to directory (one file per witness)
  *
+ * @param path_to_data os.Path object that points to data directory
  * @return Indexed sequence of lists of strings (token lists)
  */
-def read_data(): IndexedSeq[List[String]] =
-  val datafiles = os.pwd / "src" / "main" / "data" / "darwin"
-  os.walk(datafiles)
+def read_data(path_to_data: Path): IndexedSeq[List[String]] =
+  os.walk(path_to_data)
     .sorted
     .map(os.read)
     .map(_.split(raw"\s+").toList)
 
-@main def main(): Unit =
-  val token_lists = read_data()
-  val token_array = token_lists
+def create_token_array(token_lists: IndexedSeq[List[String]]): List[String] =
+  token_lists
     .head ++ token_lists
     .tail
     .zipWithIndex
     .flatMap((e, index) => List(s" #$index ") ++ e)
+
+@main def main(): Unit =
+  val path_to_darwin = os.pwd / "src" / "main" / "data" / "darwin"
+  val token_lists = read_data(path_to_darwin)
+  val token_array = create_token_array(token_lists)
   token_array.foreach(println)
