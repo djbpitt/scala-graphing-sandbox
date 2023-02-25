8d78e4c607ee036cd4466a73aeb50573018f5d70
David J Birnbaum <djbpitt@gmail.com>
2023-02-25 17:03:01 -0500

    Begin to vectorize token array
    Extract term mapping using mutable map of string to int and fold left
    TODO: Use map to create Array[Int] required by suffix array

--- src/main/scala/reptilian/reptilian.scala
+++ src/main/scala/reptilian/reptilian.scala
@@ -2,6 +2,8 @@ package reptilian
 
 import os.Path
 
+import scala.collection.mutable
+
 /** Read data files from supplied path to directory (one file per witness)
  *
  * @param path_to_data os.Path object that points to data directory
@@ -20,8 +22,15 @@ def create_token_array(token_lists: IndexedSeq[List[String]]): List[String] =
     .zipWithIndex
     .flatMap((e, index) => List(s" #$index ") ++ e)
 
+def vectorize_token_array(token_array: List[String]): mutable.Map[String, Int] =
+  token_array.foldLeft(mutable.Map[String, Int]())((term_mapping, next_term) => {
+    term_mapping.getOrElseUpdate(next_term, term_mapping.size)
+    term_mapping
+  })
+
 @main def main(): Unit =
   val path_to_darwin = os.pwd / "src" / "main" / "data" / "darwin"
   val token_lists = read_data(path_to_darwin)
   val token_array = create_token_array(token_lists)
-  token_array.foreach(println)
+  val token_terms = vectorize_token_array(token_array)
+  token_terms.foreach(println)
