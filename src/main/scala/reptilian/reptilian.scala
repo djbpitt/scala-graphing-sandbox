0ab7539be4ceab893a686b0f7f8a7bacd36aff15
David J Birnbaum <djbpitt@gmail.com>
2023-02-26 10:13:08 -0500

    Normalize function to lowercase witness data
    Normalize witness data before tokenization
    
    TODO:
    1. Create token array with separators
    2. Vectorize token array
    3. Use HOF to allow user to specify normalization rules

--- src/main/scala/reptilian/reptilian.scala
+++ src/main/scala/reptilian/reptilian.scala
@@ -26,6 +26,15 @@ def tokenize(witness_data: String, token_pattern: Regex): List[String] =
   val re = token_pattern
   re.findAllIn(witness_data).toList
 
+/** Normalize witness data
+ *
+ * @param witness_data String with data for individual witness
+ * @return Input string in all lower case
+ *
+ * TODO: Allow user to specify normalization rules
+ */
+def normalize(witness_data: String): String =
+  witness_data.toLowerCase
 
 def create_token_array(token_lists: IndexedSeq[List[String]]): List[String] =
   token_lists
@@ -44,7 +53,9 @@ def vectorize_token_array(token_array: List[String]): mutable.Map[String, Int] =
   val path_to_darwin = os.pwd / "src" / "main" / "data" / "darwin"
   val token_pattern: Regex = """\w+\s*|\W+""".r // From CollateX Python, syntax adjusted for Scala
   val witness_strings = read_data(path_to_darwin) // One string per witness
-  val witness_tokens = witness_strings.map(tokenize(_, token_pattern)) // List of one list of strings per witness
+  val witness_tokens = witness_strings
+    .map(normalize)
+    .map(tokenize(_, token_pattern)) // List of one list of strings per witness
   witness_tokens.foreach(println)
 //  val token_terms = vectorize_token_array(token_array)
 //  token_terms.foreach(println)
