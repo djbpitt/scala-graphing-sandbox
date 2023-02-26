b1aa64583e5ff1871e95f3ec533fc50130bb49fa
David J Birnbaum <djbpitt@gmail.com>
2023-02-26 10:02:53 -0500

    Specify tokenization regex
    1. read_data() reads the files into individual strings but no longer tokenizes them.
    2. New tokenize() function breaks string (all text from one witness) into tokens according to supplied regex.
    3. Regex for tokenization is supplied inside main().
    4. Currently prints token lists for each witness for diagnostic purposes.
    5. create_token_array() and vectorize_token_array() currently are not used, pending updates.
    
    TODO:
    
    1. Normalize before tokenization
    2. Create token array with separators
    3. Vectorize token array

--- src/main/scala/reptilian/reptilian.scala
+++ src/main/scala/reptilian/reptilian.scala
@@ -3,17 +3,29 @@ package reptilian
 import os.Path
 
 import scala.collection.mutable
+import scala.util.matching.Regex
 
 /** Read data files from supplied path to directory (one file per witness)
  *
  * @param path_to_data os.Path object that points to data directory
  * @return Indexed sequence of lists of strings (token lists)
  */
-def read_data(path_to_data: Path): IndexedSeq[List[String]] =
+def read_data(path_to_data: Path): List[String] =
   os.walk(path_to_data)
     .sorted
+    .toList
     .map(os.read)
-    .map(_.split(raw"\s+").toList)
+
+/** Tokenize individual witness on supplied regex matching pattern
+ *
+ * @param witness_data Individual witness as string
+ * @param token_pattern Regex matching individual tokens
+ * @return List of strings for single witness
+ */
+def tokenize(witness_data: String, token_pattern: Regex): List[String] =
+  val re = token_pattern
+  re.findAllIn(witness_data).toList
+
 
 def create_token_array(token_lists: IndexedSeq[List[String]]): List[String] =
   token_lists
@@ -30,7 +42,9 @@ def vectorize_token_array(token_array: List[String]): mutable.Map[String, Int] =
 
 @main def main(): Unit =
   val path_to_darwin = os.pwd / "src" / "main" / "data" / "darwin"
-  val token_lists = read_data(path_to_darwin)
-  val token_array = create_token_array(token_lists)
-  val token_terms = vectorize_token_array(token_array)
-  token_terms.foreach(println)
+  val token_pattern: Regex = """\w+\s*|\W+""".r // From CollateX Python, syntax adjusted for Scala
+  val witness_strings = read_data(path_to_darwin) // One string per witness
+  val witness_tokens = witness_strings.map(tokenize(_, token_pattern)) // List of one list of strings per witness
+  witness_tokens.foreach(println)
+//  val token_terms = vectorize_token_array(token_array)
+//  token_terms.foreach(println)
