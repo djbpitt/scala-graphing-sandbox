f64a636cf83fc91cfddda0bcebecb2890f192e4c
rhdekker <ronald.dekker@di.huc.knaw.nl>
2023-03-06 19:28:45 +0100

    Initial work to write the lcp array to lcp intervals function in a functional programming style.

---
+++ src/main/scala/reptilian/func_lcp_intervals.scala
@@ -0,0 +1,21 @@
+package reptilian
+
+// lets try to implement it in a functional style
+// work in progress
+def split_lcp_array_into_intervals(lcp: Array[Int]): Unit =
+  val x = lcp.zipWithIndex
+  println(x.mkString("Array(", ", ", ")"))
+
+  val f = (x1:List[Block], y1: (Int, Int)) =>
+    println(s"$x1 $y1")
+    x1
+
+  val y = x.foldLeft[List[Block]](List[Block]())(f)
+  for xx <- y do
+    println(xx)
+
+
+
+@main def functional_lcp_intevals(): Unit =
+  val lcp = Array[Int](-1, 0, 2, 3, 3, 3, 5, 2)
+  split_lcp_array_into_intervals(lcp)
