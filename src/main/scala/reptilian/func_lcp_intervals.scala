592a4a5f09543d22bac0e1d8ee60d604bc771d6e
rhdekker <ronald.dekker@di.huc.knaw.nl>
2023-03-06 22:37:00 +0100

    Some more work. Pattern match list. Minor: fixed name of main method.

--- src/main/scala/reptilian/func_lcp_intervals.scala
+++ src/main/scala/reptilian/func_lcp_intervals.scala
@@ -8,8 +8,10 @@ def split_lcp_array_into_intervals(lcp: Array[Int]): Unit =
 
   val f = (x1:List[Block], y1: (Int, Int)) =>
     println(s"$x1 $y1")
+    //NOTE: can I extract LCP value outside of the match? Probably
     (x1, y1._1) match
       case (Nil, lcp_value) if lcp_value > 0 => List(Block(y1._2, -1, lcp_value))
+      case (Block(_,_, current_value)::tail, lcp_value) if lcp_value > current_value => List() //TODO add to list
       case _ => x1
 
   val y = x.foldLeft[List[Block]](List[Block]())(f)
@@ -18,6 +20,6 @@ def split_lcp_array_into_intervals(lcp: Array[Int]): Unit =
 
 
 
-@main def functional_lcp_intevals(): Unit =
+@main def functional_lcp_intervals(): Unit =
   val lcp = Array[Int](-1, 0, 2, 3, 3, 3, 5, 2)
   split_lcp_array_into_intervals(lcp)
