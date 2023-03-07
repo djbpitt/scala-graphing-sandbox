3fe0014c1de221d50cbe04f5156cc54c24dcfb49
djbpitt <djbpitt@gmail.com>
2023-03-07 15:33:02 -0500

    Initialize test suite for reptilian

---
+++ src/test/scala/reptilian/ReptilianTest.scala
@@ -0,0 +1,13 @@
+package reptilian
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class ReptilianTest extends AnyFunSuite:
+
+  test("fake test") {
+    val result = 1
+    assert(result == 1)
+  }
+
+end ReptilianTest
+
