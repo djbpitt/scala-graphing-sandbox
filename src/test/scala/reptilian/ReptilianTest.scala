5eb8e634b6b4589b7b58fcbdb04f32b7c21c76d4
djbpitt <djbpitt@gmail.com>
2023-03-07 15:48:10 -0500

    Remove fake reptilian test and add first real test
    Verify that an lcp array with all zero values returns no intervals

--- src/test/scala/reptilian/ReptilianTest.scala
+++ src/test/scala/reptilian/ReptilianTest.scala
@@ -4,10 +4,15 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class ReptilianTest extends AnyFunSuite:
 
-  test("fake test") {
-    val result = 1
-    assert(result == 1)
+  // tests for lcp interval
+  /** lcp array always starts with -1
+   *  If all other values are 0 should find no lcp intervals
+   *  Could happen if witnesses are dissimilar and there is no repetition
+   */
+  test("lcp array with all zero values should return no intervals") {
+    val input = Array[Int](-1, 0, 0, 0, 0, 0)
+    val result = splitLCP_ArrayIntoIntervals(input)
+    assert(result.isEmpty)
   }
-
 end ReptilianTest
 
