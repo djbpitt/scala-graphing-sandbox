122ecf297b582fb2a503a01e4209869a13592886
djbpitt <djbpitt@gmail.com>
2023-07-18 15:47:45 -0400

    Create custom constructor to create LeafNode

--- src/main/scala/reptilian/tree.scala
+++ src/main/scala/reptilian/tree.scala
@@ -3,19 +3,38 @@ package reptilian
 
 import scala.collection.mutable.ListBuffer
 
+// One map entry per witness, from witness id to start and end (inclusive "to") offset in global token array
+type WitnessReadings = Map[Int, (Int, Int)] // type alias
+
 sealed trait AlignmentTreeNode // supertype of all nodes
 
-final case class RootNode(witness_count: Int, children: ListBuffer[AlignmentTreeNode] = ListBuffer.empty) extends AlignmentTreeNode
+case class RootNode(witness_count: Int, children: ListBuffer[AlignmentTreeNode] = ListBuffer.empty) extends AlignmentTreeNode
 
+case class LeafNode(witness_readings: WitnessReadings) extends AlignmentTreeNode
 
-final case class LeafNode(witness_readings: WitnessReadings) extends AlignmentTreeNode
+/** Custom constructor to simplify creation of LeafNode
+ *
+ * Input is a varargs of (Int, (Int, Int))
+ * Constructor converts it to a Map, which is wraps in LeafNode
+ * Can create new LeafNode as:
+ *    LeafNode(1 -> (2, 3), 4 -> (5, 6))
+ * */
+object LeafNode {
+  def apply(m: (Int, (Int, Int))*): LeafNode =
+    LeafNode(m.toMap)
+}
 
-final case class BranchingNode(children: ListBuffer[AlignmentTreeNode] = ListBuffer.empty) extends AlignmentTreeNode
+case class BranchingNode(children: ListBuffer[AlignmentTreeNode] = ListBuffer.empty) extends AlignmentTreeNode
 
-final case class UnexpandedNode(witness_readings: WitnessReadings) extends AlignmentTreeNode
+case class UnexpandedNode(witness_readings: WitnessReadings) extends AlignmentTreeNode
 
-// One map entry per witness, from witness id to start and end (inclusive, so "to") offset in global token array
-type WitnessReadings = Map[Int, (Int, Int)]
+def show(node: AlignmentTreeNode): Unit =
+  node match {
+    case RootNode(witness_count, children) => println(children)
+    case LeafNode(witness_readings) => println(witness_readings)
+    case BranchingNode(children) => println(children)
+    case UnexpandedNode(witness_readings) => println(witness_readings)
+  }
 
 def tree(witness_count: Int) =
   val root = RootNode(witness_count)
@@ -24,5 +43,8 @@ def tree(witness_count: Int) =
 @main
 def build_tree(): Unit =
   val t = tree(3)
-  t.children ++= List(LeafNode(Map(1 -> (100, 101), 3 -> (300, 301))), LeafNode(Map(2 -> (200, 201), 4 -> (400, 401))))
-  println(t.children)
+  t.children ++= List(
+    LeafNode(1 -> (100, 101), 2 -> (200, 201)),
+    LeafNode(3 -> (300, 301), 4 -> (400, 401))
+  )
+  show(t)
