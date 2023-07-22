b222c190c9e16f15d0a825caed3b3a710614ec60
djbpitt <djbpitt@gmail.com>
2023-07-22 17:18:34 -0400

    Render leaf nodes with witness identifiers in alignment graph

--- src/main/scala/reptilian/tree.scala
+++ src/main/scala/reptilian/tree.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable
 import scala.collection.mutable.{ListBuffer, Queue}
 
 // One map entry per witness, from witness id to start and end (inclusive "to") offset in global token array
-type WitnessReadings = Map[Int, (Int, Int)] // type alias
+type WitnessReadings = Map[String, (Int, Int)] // type alias
 
 sealed trait AlignmentTreeNode // supertype of all nodes
 
@@ -24,7 +24,7 @@ final case class LeafNode(witness_readings: WitnessReadings) extends AlignmentTr
  * Catch and report empty parameter, which is always a mistake because leaf nodes cannot be empty
  * */
 object LeafNode {
-  def apply(m: (Int, (Int, Int))*): AlignmentTreeNode =
+  def apply(m: (String, (Int, Int))*): AlignmentTreeNode =
     if m.isEmpty then
       StringNode("Empty leaf node not allowed")
     else
@@ -52,31 +52,34 @@ def dot(root: BranchingNode): String =
   var id = 0
   val nodes_to_process: mutable.Queue[(Int, AlignmentTreeNode)] = mutable.Queue((id, root))
   val edges = ListBuffer[String]() // Not List because we append to maintain order
-  val nodes = ListBuffer[String]() // Store node attributes where needed
+  val string_nodes = ListBuffer[String]() // Store node attributes where needed
+  val leaf_nodes = ListBuffer[String]()
   while nodes_to_process.nonEmpty do
     val current_node = nodes_to_process.dequeue()
     current_node match {
-      case (_, BranchingNode(children)) =>
+      case (current_id, BranchingNode(children)) =>
         for i <- children do {
           id += 1
           nodes_to_process.enqueue((id, i))
           edges.append(List(current_node._1, " -> ", id).mkString(" "))
         }
-      case (_, LeafNode(witness_readings)) =>
-        for i <- witness_readings do {
-          id += 1
-          edges.append(List(current_node._1, " -> ", id).mkString(" "))
-        }
-      case (_, StringNode(txt)) =>
+      case (current_id, LeafNode(witness_readings)) =>
+        leaf_nodes.append(List(current_id.toString, " ", witness_readings.keys.mkString(",")).mkString(""))
+      case (current_id, StringNode(txt)) =>
         id += 1
         edges.append(List(current_node._1, " -> ", id).mkString(" "))
-        nodes.append(id.toString)
+        string_nodes.append(id.toString)
       case _ => ()
     }
-  println("Nodes: ")
-  val formatted_nodes = nodes
+  val formatted_string_nodes = string_nodes
     .map(e => List(e, " [style=filled, fillcolor=pink]").mkString("")).mkString("\n")
-  List(header, formatted_nodes, edges.mkString("\n\t"), footer).mkString("\n")
+  val formatted_leaf_nodes = leaf_nodes
+    .map(e =>
+      val split: Array[String] = e.split(" ")
+      List(split(0), " [label=\"", split(0), " (", split(1), ")\"]").mkString("")
+    )
+    .mkString("\n")
+  List(header, edges.mkString("\n\t"), formatted_string_nodes, formatted_leaf_nodes, footer).mkString("\n")
 
 def tree(witness_count: Int) =
   val root = BranchingNode()
@@ -86,13 +89,27 @@ def tree(witness_count: Int) =
 def build_tree(): Unit =
   val t = tree(3)
   t.children ++= List(
-    LeafNode(1 -> (100, 101), 2 -> (200, 201)),
-    LeafNode(3 -> (300, 301), 4 -> (400, 401)),
-    BranchingNode(children=ListBuffer(LeafNode(5 -> (500, 501)))),
-    LeafNode()
+    LeafNode("w0" -> (100, 101), "w1" -> (200, 201)),
+    BranchingNode(children = ListBuffer(LeafNode("w0" -> (500, 501)), LeafNode("w1" -> (600, 601)))),
+    LeafNode("w0" -> (700, 701), "w1" -> (800, 801)),
+    BranchingNode(children = ListBuffer(LeafNode("w1" -> (900, 901)), LeafNode("w0" -> (1000, 1001)))),
+    LeafNode("w0" -> (1100, 1101), "w1" -> (1200, 1201))
   )
   val dot_result = dot(t)
   val graphOutputPath = os.pwd / "src" / "main" / "output" / "alignment.dot"
   os.write.over(graphOutputPath, dot_result)
   println(dot_result)
 
+/* Sample data
+w0: The black and the red cat
+w1: The red and the black cat
+
+Blocks:
+
+The (w0, w1)
+red (w0) ~ black (w1)
+and the (w0, w1)
+black (w1) ~ red (w0)
+cat (w0, w1)
+
+* */
