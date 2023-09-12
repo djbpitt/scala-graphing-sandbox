18d8632f923336d099abbeeeffbff474cc011363
djbpitt <djbpitt@gmail.com>
2023-09-12 16:52:43 -0400

    Move conversion of full-depth blocks to reading nodes into tree.scala
    Delete separate createAlignment.scala file, now empty

--- src/main/scala/net/collatex/reptilian/tree.scala
+++ src/main/scala/net/collatex/reptilian/tree.scala
@@ -54,3 +54,22 @@ def show(node: AlignmentTreeNode): Unit =
 def tree(witnessCount: Int) =
   val root = RootNode()
   root
+
+/** Input is Vector[Int], representing FullDepthBlock instances
+ * Output is Vector[AlignmentNode], where the nodes are all of
+ * type ReadingNode
+ *
+ * Will need to deal with non-full-depth locations in the alignment
+ * */
+
+def blocksToNodes(blocks: Iterable[FullDepthBlock]): Iterable[ReadingNode] =
+  blocks
+    .map(fullDepthBlockToReadingNode)
+def fullDepthBlockToReadingNode(block: FullDepthBlock): ReadingNode =
+  val readings = block.instances
+    .zipWithIndex
+    .map((start, witnessNo) =>
+      "w" + witnessNo.toString -> Tuple2(start, start + block.length))
+    .toMap
+  ReadingNode(readings)
+
