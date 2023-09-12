1ebd973309280754afe8f3935fa7850e293c88bb
djbpitt <djbpitt@gmail.com>
2023-09-12 16:23:15 -0400

    Replace snake case with camelCase in scala filenames in net.collatex.util

--- src/main/scala/net/collatex/util/scalingExperiment.scala
+++ src/main/scala/net/collatex/util/scalingExperiment.scala
@@ -1,5 +1,8 @@
+package net.collatex.util
+
 import smile.feature.transform.WinsorScaler
 import smile.data.DataFrame
+
 /*
 * Roll your own StandardScaler (= we are not doing this)
 * https://sebastianraschka.com/Articles/2014_about_feature_scaling.html#standardization-and-min-max-scaling
@@ -13,7 +16,7 @@ z_scores = [(x_i - mean)/std_dev for x_i in x]
 */
 
 // https://stackoverflow.com/questions/64382334/smile-scala-api-create-dataframe-from-array/64411258#64411258
-@main def scaling_practice: Unit =
+@main def scaling_practice(): Unit =
   val data = Array(
     Array(1.0, 2.0),
     Array(4.0, 3.0),
