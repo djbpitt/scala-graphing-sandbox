c1dcdd49799ec5827e743be139091b9e35e170ba
djbpitt <djbpitt@gmail.com>
2023-09-16 17:11:50 -0400

    Move creation of alignment table into createAlignment.scala

--- src/main/scala/net/collatex/reptilian/createAlignment.scala
+++ src/main/scala/net/collatex/reptilian/createAlignment.scala
@@ -20,7 +20,7 @@ import scalax.collection.io.dot.*
 import scalax.collection.io.dot.implicits.toId
 import scalax.collection.io.dot.implicits.toNodeId
 
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 val endNodeId = Integer.MAX_VALUE  // End-node uses maximum possible integer value (i.e., inconveniently large value)
 implicit val myConfig: CoreConfig = CoreConfig()
@@ -328,4 +328,108 @@ def alignmentIntsToBlocks(alignment: Set[Int], blocks: Iterable[FullDepthBlock])
   val alignmentBlocks: Iterable[FullDepthBlock] = blocks
     .filter(e => alignment.contains(e.instances.head))
   alignmentBlocks
+
+// Find blocks (vectorize, create suffix array and lcp array, create blocks, find depth)
+def createAlignment(witnessStrings: List[String])(implicit tokenArray: Vector[Token]) =
+  val (allBlocks, tmpSuffixArray, longestFullDepthNonrepeatingBlocks) = createAlignedBlocks(tokenArray, witnessStrings.size)
+  implicit val suffixArray: Array[Int] = tmpSuffixArray
+
+  val blockTexts: Map[Int, String] = blockTextById(longestFullDepthNonrepeatingBlocks, tokenArray)
+
+  val blockRangeSeq = createRangedSeq(allBlocks) // Finger tree
+
+  // create navigation graph and filter out transposed nodes
+  val graph = createTraversalGraph(longestFullDepthNonrepeatingBlocks)
+
+  val alignment: List[Int] = findOptimalAlignment(graph) // Int identifiers of full-depth blocks
+  val alignmentBlocksSet: Set[Int] = alignmentBlocksAsSet(alignment: List[Int])
+  val alignmentBlocks: Iterable[FullDepthBlock] = alignmentIntsToBlocks(alignmentBlocksSet, longestFullDepthNonrepeatingBlocks)
+
+
+  val readingNodes = blocksToNodes(alignmentBlocks)
+  var root = RootNode()
+  val sortedReadingNodes = readingNodes // Sort reading nodes in token order
+    .toVector
+    .sortBy(_.witnessReadings("w0")._1)
+  val sigla = sortedReadingNodes.head.witnessReadings.keys.toList // Humiliating temporary step
+  /* For each sliding pair of reading nodes create an unexpanded node with witness readings
+  *   that point from each siglum to a slice from the end of the first reading node to the
+  *   start of the second. */
+  val unalignedIntermediates = sortedReadingNodes
+    .sliding(2)
+    .map(pair =>
+      val mapEntries = sigla
+        .map(siglum => siglum -> (pair.head.witnessReadings(siglum)(1), pair(1).witnessReadings(siglum)(0)))
+        .toMap
+      UnexpandedNode(mapEntries.filterNot(e => e._2._1 == e._2._2))
+    )
+  // Used to check for unaligned leading or trailing tokens
+  // Possibly unnecessary traversal of token array
+  // Can we find the first and last tokens of each witness without a separate traversal?
+  val boundaryTokens =
+  tokenArray
+    .map(_.t)
+    .zipWithIndex
+    .filter(e => e._1.contains(" #"))
+    .map(_._2)
+  val firstTokens = Vector(0) ++ boundaryTokens.map(_ + 1)
+  val lastTokens = boundaryTokens.map(_ - 1) ++ Vector(tokenArray.size - 1)
+  val leadingTokens = sigla
+    .sorted
+    .zipWithIndex
+    .map(e => e._1 -> (firstTokens(e._2), sortedReadingNodes.head.witnessReadings(e._1)(0)))
+    .toMap
+  val leadingDeltas: Boolean = leadingTokens
+    .values
+    .map(e => e._2 - e._1)
+    .sum != 0
+  val leadingUnexpanded: Option[UnexpandedNode] =
+    if leadingDeltas then
+      Some(UnexpandedNode(leadingTokens))
+    else
+      None
+  val trailingTokens = sigla
+    .sorted
+    .zipWithIndex
+    .map(e => e._1 -> (sortedReadingNodes.last.witnessReadings(e._1)(1), lastTokens(e._2)))
+    .toMap
+  val trailingDeltas: Boolean = trailingTokens
+    .values
+    .map(e => e._2 + 1 - e._1) // Range points *after* last token, so add 1
+    .sum != 0
+  val trailingUnexpanded: Option[UnexpandedNode] =
+    if trailingDeltas then
+      Some(UnexpandedNode(trailingTokens))
+    else
+      None
+  val readingAndIntermediateNodes = sortedReadingNodes
+    .zip(unalignedIntermediates)
+    .flatMap(_.toList) ++ List(sortedReadingNodes.last)
+
+  val allBlockRanges = allBlocks
+    .map(
+      (b: Block) =>
+        (suffixArray.slice(b.start, b.end).toList, b.length)
+    )
+    .map(e => e._1.map(f => (f, f + e._2)))
+  //  allBlockRanges.foreach(println)
+
+  val newChildren: ListBuffer[AlignmentTreeNode] =
+    ListBuffer(leadingUnexpanded).flatten
+  newChildren.appendAll(readingAndIntermediateNodes)
+  newChildren.appendAll(List(trailingUnexpanded).flatten)
+  root = RootNode(newChildren)
+
+  val newerChildren =
+    newChildren.map {
+      case e: UnexpandedNode =>
+        val nodeRanges = e.witnessReadings.values
+        allBlockRanges.filter(_.size == nodeRanges.size)
+      case e: ReadingNode => "R"
+      case _ => "Oops" // Shouldn't happen
+    }
+  // Diagnostic (temporary)
+  visualizeTraversalGraph(graph, blockTexts, alignmentBlocksSet)
+
+  (root, sigla)
 
