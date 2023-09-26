16e6b24295b445458b451817a7ae78cbb831a6f0
djbpitt <djbpitt@gmail.com>
2023-09-26 17:17:47 -0400

    Create LocalToken case class
    Break out properties shared by global and local tokens into TokenProperties trait
    Functions (3) that previously required tokenArray: Vector[Token] now require Vector[TokenProperties]

--- src/main/scala/net/collatex/reptilian/tokenization.scala
+++ src/main/scala/net/collatex/reptilian/tokenization.scala
@@ -3,6 +3,13 @@ package net.collatex.reptilian
 import scala.collection.mutable.ArrayBuffer
 import scala.util.matching.Regex
 
+/** Properties shared by Token and LocalToken */
+trait TokenProperties {
+  def t: String
+
+  def n: String
+  def w: Int
+}
 /** Token as complex object
  *
  * @param t Raw token, which may include trailing whitespace
@@ -11,7 +18,17 @@ import scala.util.matching.Regex
  *
  *          Tokenization and normalization are under user control (to be implement)
  */
-case class Token(t: String, n: String, w: Int)
+case class Token(t: String, n: String, w: Int) extends TokenProperties
+
+/** Local token has t, n, w, and offset position in original token array
+ *
+ * @param t Raw token, which may include trailing whitespace
+ * @param n Normalized token, e.g., lower-case and trim
+ * @param w Witness identifier, zero-based
+ * @param g Offset into global token array
+ *
+ */
+case class LocalToken(t: String, n: String, w: Int, g: Int) extends TokenProperties
 
 /** Used as partially applied function to create tokenizer
  *
