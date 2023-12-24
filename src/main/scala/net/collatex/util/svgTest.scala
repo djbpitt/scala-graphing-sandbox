42f05b73466d26546e7ad91d0cbfb6586e70818e
djbpitt <djbpitt@gmail.com>
2023-12-24 14:48:31 -0500

    Layout progress for alluvial diagram
    Includes reading and indel nodes
    TODO: variation nodes; impeded until we determine structure
    TODO: Sigmoid connections
    TODO: Review colors, sizes
    
    Should gap between nodes be a function of total witness count?

--- src/main/scala/net/collatex/util/svgTest.scala
+++ src/main/scala/net/collatex/util/svgTest.scala
@@ -4,10 +4,11 @@ import scala.xml.Elem
 import scala.xml.XML.save
 import annotation.tailrec
 import scala.jdk.CollectionConverters.*
+import net.collatex.reptilian.{AlignmentTreeNode, IndelNode, ReadingNode, VariationNode}
 
 object WitnessColors extends Enumeration {
   type witnessColor = Value
-  val Red, Orange, Yellow, Green, Blue, Indigo, lightgray = Value
+  val Peru, Orange, Yellow, LimeGreen, DodgerBlue, Violet = Value
 }
 import WitnessColors._
 
@@ -17,18 +18,10 @@ object Sigla extends Enumeration {
 }
 import Sigla._
 
-val nodes = Vector(
-  Node(readings = Map(W66 -> "causes of variability .", W69 -> "causes of variability .", W72 -> "causes of variability .")),
-  Node(readings = Map(W59 -> "when we", W60 -> "when we", W61 -> "when we", W66 -> "when we", W69 -> "when we", W72 -> "when we")),
-  Node(readings = Map(W59 -> "look to", W60 -> "look to", W61 -> "look to", W66 -> "look to", W69 -> "compare", W72 -> "compare")),
-  Node(readings = Map(
-    W59 -> "the individuals of the same variety or sub - variety of our older cultivated plants and animals , one of the first points which strikes us",
-    W60 -> "the individuals of the same variety or sub - variety of our older cultivated plants and animals , one of the first points which strikes us",
-    W61 -> "the individuals of the same variety or sub - variety of our older cultivated plants and animals , one of the first points which strikes us",
-    W66 -> "the individuals of the same variety or sub - variety of our older cultivated plants and animals , one of the first points which strikes us",
-    W69 -> "the individuals of the same variety or sub - variety of our older cultivated plants and animals , one of the first points which strikes us",
-    W72 -> "the individuals of the same variety or sub - variety of our older cultivated plants and animals , one of the first points which strikes us"
-  ))
+val nodes: Vector[AlignmentTreeNode] = Vector(
+  ReadingNode(witnessReadings = Map("W59" -> (0, 1), "W60" -> (2, 3), "W61" -> (4, 5), "W66" -> (6, 7), "W69" -> (8, 9), "W72" -> (10, 11))),
+  IndelNode(witnessReadings = Map("W66" -> (6, 7), "W69" -> (8, 9), "W72" -> (10, 11))),
+  ReadingNode(witnessReadings = Map("W59" -> (0, 1), "W60" -> (2, 3), "W61" -> (4, 5), "W66" -> (6, 7), "W69" -> (8, 9), "W72" -> (10, 11)))
 )
 
 @tailrec
@@ -40,21 +33,111 @@ def processReadingGroups(rgs: Map[String, Map[Sigla.Value, String]], pos: Int, a
     val newItem: Elem = <rect></rect>
     processReadingGroups(rgs.tail, pos + currentRg._2.size, acc :+ newItem)
 
+/** Compute <path> elements from preceding node to current node
+ *
+ * One <path> per witness
+ * Witness groupings and order (order of groups and order of witnesses within groups) of both nodes must be known
+ * All witnesses are present in all output, either as explicit or, if absent from node, as inferred
+ *
+ * @param source as Node
+ * @param target as Node
+ * @return <g> with <path> children, one for each witness
+ */
+
+def computePath(source: AlignmentTreeNode, target: AlignmentTreeNode): Elem =
+  ???
+
+/* Create SVV for output
+ *
+ * Input is sequence of nodes
+ * Output has two parts:
+ *   1. Sigmoid connections from preceding node to current node (absent for first node)
+ *      <g> with <path> children
+ *   2. Current node <g> with <rect> children
+ *
+ */
 val svg: Elem =
+  /* Constants */
+  val witW = 10
+  val witH = 10
+  val verticalNodeSpacing = 3 * witH // height of node plus twice height of node for sigmoid connectors
+  val totalWitCount = Sigla.values.size
+  /* End of constants*/
+  /* Vertical line between present and absent witnesses first */
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 180">
-    {
-    val nodesWithIndex = nodes.zipWithIndex
-    for (n, i) <- nodesWithIndex yield
-      val translateInstruction = "translate(" + i + ", 10)"
-      val readingGroups = n.readings.groupBy((_, text) => text)
-      val rects = processReadingGroups(readingGroups, 0, Vector.empty)
-      <g transform={translateInstruction}> { rects } </g>
-    }
+    <g transform="translate(10)">
+      <line
+      transform={"translate(0, -" + ((verticalNodeSpacing - witH) / 2).toString + ")"}
+      x1={(totalWitCount * witW + witW / 2).toString}
+      y1="0"
+      x2={(totalWitCount * witW + witW / 2).toString}
+      y2={(nodes.size * verticalNodeSpacing).toString}
+      stroke="gray"/>{val nodesWithIndex = nodes.zipWithIndex
+    for (n, i) <- nodesWithIndex yield {
+      val translateInstruction = "translate(0, " + (i * verticalNodeSpacing).toString + ")"
+      val contents: Vector[Elem] = n match
+        case ReadingNode(witnessReadings) =>
+          val rects: Vector[Elem] =
+            witnessReadings.keys.toSeq.sorted // Sort alphabetically by siglum
+              .zipWithIndex
+              .toVector
+              .flatMap((siglum, offset) => {
+                val xPos: String = (offset * witW).toString
+                val fill: String = WitnessColors(offset).toString
+                Vector(<rect x={xPos} y="0" width={witW.toString} height={witH.toString} fill={fill}/>,
+                  <text x={(xPos.toInt + witW / 2).toString}
+                        y={(witH / 2).toString}
+                        text-anchor="middle"
+                        dominant-baseline="central"
+                        font-size={(witH / 2).toString}>
+                    {siglum.drop(1)}
+                  </text>)
+              })
+          rects
+        case IndelNode(witnessReadings) =>
+          val readingRects: Vector[Elem] =
+            witnessReadings.keys.toSeq.sorted
+              .zipWithIndex
+              .toVector
+              .flatMap((siglum, offset) => {
+                val xPos: String = (offset * witW).toString
+                val fill: String = WitnessColors(Sigla.withName(siglum).id).toString
+                Vector(<rect x={xPos} y="0" width={witW.toString} height={witH.toString} fill={fill}/>,
+                  <text x={(xPos.toInt + witW / 2).toString}
+                        y={(witH / 2).toString}
+                        text-anchor="middle"
+                        dominant-baseline="central"
+                        font-size={(witH / 2).toString}>
+                    {siglum.drop(1)}
+                  </text>)
+              })
+          val missingRects: Vector[Elem] =
+            Sigla.values.filterNot(e => witnessReadings.keySet.contains(e.toString))
+              .zipWithIndex
+              .toVector
+              .flatMap((siglum, offset) => {
+                val xPos: String = ((offset + Sigla.values.size + 1) * witW).toString // include spacer
+                val fill: String = WitnessColors(Sigla.withName(siglum.toString).id).toString
+                Vector(<rect x={xPos} y="0" width={witW.toString} height={witH.toString} fill={fill}/>,
+                  <text x={(xPos.toInt + witW / 2).toString}
+                        y={(witH / 2).toString}
+                        text-anchor="middle"
+                        dominant-baseline="central"
+                        font-size={(witH / 2).toString}>
+                    {siglum.toString.drop(1)}
+                  </text>)
+              })
+          readingRects :++ missingRects
+        case _ => Vector(<rect></rect>)
+      <g transform={translateInstruction}>
+        {contents}
+      </g>
+    }}
+    </g>
   </svg>
 
 @main def testSvg(): Unit =
   val pp = new scala.xml.PrettyPrinter(78, 2)
   val x = pp.format(svg)
   println(x)
-
-case class Node(readings: Map[Sigla.Value, String])
+  save("svgTest.svg", svg)
