f82cd6db6121af74e0aa71800b1626b60c1a27f3
djbpitt <djbpitt@gmail.com>
2024-01-01 14:56:44 -0500

    Reading groups within variation node are sorted correctly, but horizontal connecting lines are wrong

--- src/main/scala/net/collatex/util/svgTest.scala
+++ src/main/scala/net/collatex/util/svgTest.scala
@@ -237,7 +237,7 @@ private def processNodes(nodes: Vector[HasWitnessReadings]): Vector[Elem] =
         val readingGroups: Vector[Vector[String]] = currentNode.witnessReadings // vector of vectors of sigla
           .groupBy((_, offsets) => tokenArray.slice(offsets._1, offsets._2)) // groupo by same reading text
           .map((_, attestations) => attestations.keys.toVector.sorted) // keep only sigla
-          .toVector
+          .toVector.sorted
         println("\nNew group:")
         readingGroups.foreach(println)
         val readingGroupSizes = readingGroups.map(_.size)
