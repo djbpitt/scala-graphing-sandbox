2c1cd7834c4b9ec871535d66139f31b4ac2ac611
djbpitt <djbpitt@gmail.com>
2024-05-06 11:02:39 -0400

    Testing IDE configuration; ignore code changes

--- src/main/scala/net/collatex/reptilian/mixedVisualizationGrid.scala
+++ src/main/scala/net/collatex/reptilian/mixedVisualizationGrid.scala
@@ -933,7 +933,7 @@ private def createHorizontalRibbons(root: ExpandedNode, tokenArray: Vector[Token
         <style type="text/css">{css}</style>
       </head>
       <body>
-        <h1>Alignments x</h1>
+        <h1>Alignments</h1>
         <main>
           <div id="wrapper">
             <svg xmlns="http://www.w3.org/2000/svg"
