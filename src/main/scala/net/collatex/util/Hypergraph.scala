4ebfb25676648697892e24f37cec50084d1c4b81
rhdekker <ronald.dekker@di.huc.knaw.nl>
2024-07-12 20:00:20 +0200

    Added the Vertex construction method.

--- src/main/scala/net/collatex/util/Hypergraph.scala
+++ src/main/scala/net/collatex/util/Hypergraph.scala
@@ -8,4 +8,15 @@ package net.collatex.util
 // Hypergraph has hyperedges of type HE and vertices of type V
 case class Hypergraph[HE, V](am1: Map[HE, Set[V]], am2: Map[V, Set[HE]])
 
+// add concat method
 
+// add connect method
+
+
+object Hypergraph:
+  def vertex[HE, V](vertex: V): Hypergraph[HE, V] =
+    Hypergraph(Map.empty, Map.apply(vertex -> Set.empty))
+    
+  // add hyperedge method
+  
+  
