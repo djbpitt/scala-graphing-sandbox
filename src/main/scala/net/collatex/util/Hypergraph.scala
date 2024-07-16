9d2e2939c8d2cd5e8076581d40fde82f8b315356
rhdekker <ronald.dekker@di.huc.knaw.nl>
2024-07-16 14:48:16 +0200

    Simplified the hypergraph implementation a bit by removing duplication.

--- src/main/scala/net/collatex/util/Hypergraph.scala
+++ src/main/scala/net/collatex/util/Hypergraph.scala
@@ -23,11 +23,11 @@ case class Hypergraph[L, V](am1: Map[L, Set[V]], am2: Map[V, Set[L]]):
   def *(other: Hypergraph[L, V]): Hypergraph[L, V] =
     // every L of this should connect to every V of other.
     // every L of other should connect to every V of this.
-    val new_hyperedges_to_vertex_map = this.am1.map((label, vertices) => label -> (vertices union other.am2.keySet))
-      |+| other.am1.map((label, vertices) => label -> (vertices union this.am2.keySet))
+    val new_hyperedges_to_vertex_map = this.am1.map((label, vertices) => label -> (vertices union other.vertices))
+      |+| other.am1.map((label, vertices) => label -> (vertices union this.vertices))
     // And of course the second mapping (V->L) needs to be updated too
-    val new_vertices_to_label_map = this.am2.map((vertex, labels) => vertex -> (labels union other.am1.keySet))
-      |+| other.am2.map((vertex, labels) => vertex -> (labels union this.am1.keySet))
+    val new_vertices_to_label_map = this.am2.map((vertex, labels) => vertex -> (labels union other.hyperedges))
+      |+| other.am2.map((vertex, labels) => vertex -> (labels union this.hyperedges))
 
     Hypergraph(new_hyperedges_to_vertex_map, new_vertices_to_label_map)
 
