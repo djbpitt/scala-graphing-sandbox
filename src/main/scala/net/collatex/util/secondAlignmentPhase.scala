8b40dfba0ffd7d224fdb7a5a7b79a1008da94486
djbpitt <djbpitt@gmail.com>
2024-08-31 14:44:33 -0400

    Copy into secondAlignmentPhase.scala from unalignedDev.scala: function to perform nw and pattern matching on cluster type (combination of Singleton and Hypergraph)

--- src/main/scala/net/collatex/util/secondAlignmentPhase.scala
+++ src/main/scala/net/collatex/util/secondAlignmentPhase.scala
@@ -39,11 +39,12 @@ enum ClusterInfo:
   def item2: Int
   def height: Double
   case SingletonSingleton(item1: Int, item2: Int, height: Double)
-  case SingletonTree(item1: Int, item2: Int, height: Double)
-  case TreeTree(item1: Int, item2: Int, height: Double)
+  case SingletonHG(item1: Int, item2: Int, height: Double)
+  case HGHG(item1: Int, item2: Int, height: Double)
+import ClusterInfo.*
 
 object ClusterInfo:
-  // "of" is conventional name for constructor
+  // "of" is conventional name for constructor; HG = hypergraph
   def of(
       item1: Int,
       item2: Int,
@@ -53,8 +54,8 @@ object ClusterInfo:
     (item1 < witnessCount, item2 < witnessCount) match
       case (true, true) => SingletonSingleton(item1, item2, height)
       case (true, false) =>
-        SingletonTree(item1, item2, height) // Assume singleton is first
-      case (false, false) => TreeTree(item1, item2, height)
+        SingletonHG(item1, item2, height) // Assume singleton is first
+      case (false, false) => HGHG(item1, item2, height)
       case _              => throw Exception("(false, true) should not occur")
 
 def vectorizeReadings(node: List[List[Token]]): Array[Array[Double]] =
@@ -86,15 +87,57 @@ def clusterReadings(data: Array[Array[Double]]): List[ClusterInfo] =
     .map(e => ClusterInfo.of(e(0)(0), e(0)(1), e(1), data.length))
     .toList
 
+def substitutionCost[A](a: A, b: A): Double =
+  if (a == b) 0.0d else 1.0d
+
+def nwCreateMatrix(a: List[String], b: List[String]): Array[Array[Double]] =
+  val deletionCost = 1.0d
+  val insertionCost = 1.0d
+  val transpositionCost = 1.0d
+
+  val d = Array.ofDim[Double](a.size + 1, b.size + 1)
+
+  for (i <- 0 to a.size)
+    d(i)(0) = i * deletionCost
+  for (j <- 0 to b.size)
+    d(0)(j) = j * insertionCost
+  for {
+    i <- 1 to a.size
+    j <- 1 to b.size
+  } {
+    d(i)(j) = math.min(
+      math.min(
+        d(i - 1)(j) + deletionCost, // deletion
+        d(i)(j - 1) + insertionCost
+      ), // insertion
+      d(i - 1)(j - 1) + substitutionCost(a(i - 1), b(j - 1))
+    ) // substitution
+    if (i > 1 && j > 1 && a(i - 1) == b(j - 2) && a(i - 2) == b(j - 1))
+      d(i)(j) = math.min(d(i)(j), d(i - 2)(j - 2) + transpositionCost) // transposition
+  }
+  val distance = d(a.size)(b.size)
+  d // return entire matrix
+
 @main def secondAlignmentPhase(): Unit =
   val darwinReadings: List[List[Token]] = readJsonData // we know there's only one
   given tokenArray: Vector[Token] =
     darwinReadings.head.toVector ++ darwinReadings.tail.zipWithIndex
       .flatMap((e, index) => List(Token(index.toString, index.toString, index, -1)) ++ e)
       .toVector
-  println(s"tokenArray: $tokenArray")
   val nodeToClusters: List[ClusterInfo] =
     (vectorizeReadings andThen clusterReadings)(darwinReadings) // list of tuples
-  println(s"nodeToClusters: $nodeToClusters")
+  nodeToClusters map {
+    case SingletonSingleton(item1, item2, height) =>
+      // TODO: We have not yet explored Indels in SingletonSingleton patterns
+      val w1: List[Token] = darwinReadings(item1)
+      val w2: List[Token] = darwinReadings(item2)
+      val m = nwCreateMatrix(w1.map(_.n), w2.map(_.n))
+        val dfm = DataFrame.of(m) // just to look; we don't need the DataFrame
+        println(dfm.toString(dfm.size))
+      // acc(i + darwin.head.readings.size) = matrixToAlignmentTree(w1, w2)
+
+    case SingletonHG(item1, item2, height) => println(height)
+    case HGHG(item1, item2, height) => println(height)
+  }
 
 // 2024-08-29 RESUME HERE: Do smart things with list of clustering information
