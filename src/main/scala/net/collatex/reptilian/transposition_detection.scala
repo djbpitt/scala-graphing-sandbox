1edaad2b0c88a92381c101fde33f6ccecd2e341f
rhdekker <ronald.dekker@di.huc.knaw.nl>
2024-10-31 15:52:06 +0100

    Elaborated a bit on the steps.

--- src/main/scala/net/collatex/reptilian/transposition_detection.scala
+++ src/main/scala/net/collatex/reptilian/transposition_detection.scala
@@ -9,6 +9,10 @@
  * b. Go over the hyperedges in each hypergraph
  * c. Map the hyper-graphs to sorted map[Int, String], where int is the position where a token range start in the global token array
  * and String is the edge label where the data comes from
+ * d. Create the outgoing edges for the dependency graph by going over the hyperedges and using the sorted map figuring out what the next item
+ *  in the map is for the positions in each of the witnesses.
+ *  That we find the hyperedges, which are the nodes in the dependency graph to connect to. De-duplicate the edges, because when witnesses
+ *  will point to the same ones.
  * 2. Topological sort the dependency graph
  * 3. Rank the hyperedges
  * 4. Create a traversal/decision graph for the traversal of the two sorted and ranked hyperedges
