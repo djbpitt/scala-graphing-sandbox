0fb6492ba40cacfb0b2008932b950bf5122d41e4
David J Birnbaum <djbpitt@gmail.com>
2024-11-02 17:03:37 -0400

    Update test for SingleNodeGraph * SingleNodeGraph

--- src/test/scala/net/collatex/util/GraphTest.scala
+++ src/test/scala/net/collatex/util/GraphTest.scala
@@ -1,11 +1,11 @@
 package net.collatex.util
 
+import net.collatex.util.Graph.DirectedGraph
 import org.scalatest.funsuite.AnyFunSuite
 
 class GraphTest extends AnyFunSuite:
   test("create DAG"):
     val edges = Set(("173", "254"), ("173", "173"), ("192", "ends"), ("starts", "255"), ("192", "192"), ("255", "ends"))
-    val expected = Graph.empty[String]
+    val expected = DirectedGraph(Map("173" -> (Set(),Set("254")), "254" -> (Set("173"),Set())))
     val result = Graph.edge(edges.head._1, edges.head._2)
-    println(result)
-    assert(result != expected)
+    assert(result == expected)
