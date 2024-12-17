b3b968bf869c0c16de3c3d72b8c7df02c3f144e4
David J Birnbaum <djbpitt@gmail.com>
2024-12-17 16:23:40 -0500

    Fix off-by-one error in TokenRange.contains()

--- src/main/scala/net/collatex/reptilian/alignmentTree.scala
+++ src/main/scala/net/collatex/reptilian/alignmentTree.scala
@@ -27,7 +27,7 @@ enum TokenRange:
     gTa.slice(this.start, this.until).map(_.t).mkString // concatenate t values
   def decreaseStart(): TokenRange = TokenRange(this.start - 1, this.until)
   def contains(pos: Int): Boolean = // position in gTa
-    this.start <= pos && this.until >= pos
+    this.start <= pos && this.until > pos
 object TokenRange:
   def apply(start: Int, until: Int): TokenRange =
     Ordering.Int.compare(start, until) match
