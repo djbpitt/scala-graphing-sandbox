be1f4fd2703686c6a579b615857001ee5c8a8fa2
rhdekker <ronald.dekker@di.huc.knaw.nl>
2024-12-22 14:05:55 +0100

    Refactoring: introduce Hyperedge slice function, used in the split function.

--- src/main/scala/net/collatex/reptilian/alignmentHyperedge.scala
+++ src/main/scala/net/collatex/reptilian/alignmentHyperedge.scala
@@ -8,34 +8,22 @@ import net.collatex.util.Hypergraph.Hyperedge
  */
 
 extension (he: Hyperedge[EdgeLabel, TokenRange])
+  def slice(startOffset: Int, untilOffset: Int): Hypergraph[EdgeLabel, TokenRange] =
+    // TODO: Are we content with creating a bogus TokenRange on error?
+    if startOffset == untilOffset then Hypergraph.empty
+    else
+      Hyperedge(EdgeLabel(he.vertices.head.start + startOffset),
+        he.vertices.map(_.slice(startOffset, untilOffset)
+          .getOrElse(TokenRange(-1, -1)))
+      )
+
   def split(
       preLength: Int,
       blockLength: Int,
       postLength: Int
   ): Hypergraph[EdgeLabel, TokenRange] =
-    // TODO: Are we content with creating a bogus TokenRange on error?
     if preLength == 0 && postLength == 0 then he
     else
-      val pres =
-        if preLength == 0
-        then Hypergraph.empty
-        else
-          Hyperedge( // pres
-            EdgeLabel(he.vertices.head.start),
-            he.vertices.map(e => e.slice(0, preLength).getOrElse(TokenRange(-1, -1)))
-          )
-      val posts =
-        if postLength == 0
-        then Hypergraph.empty
-        else
-          Hyperedge( // posts
-            EdgeLabel(he.vertices.head.start + preLength + blockLength),
-            he.vertices.map(e =>
-              e.slice(preLength + blockLength, preLength + blockLength + postLength).getOrElse(TokenRange(-1, -1))
-            )
-          )
-      pres + posts +
-        Hyperedge( // blocks
-          EdgeLabel(he.vertices.head.start + preLength),
-          he.vertices.map(e => e.slice(preLength, preLength + blockLength).getOrElse(TokenRange(-1, -1)))
-        )
+      he.slice(0, preLength) +
+        he.slice(preLength, preLength + blockLength) +
+          he.slice(preLength + blockLength, preLength + blockLength + postLength)
