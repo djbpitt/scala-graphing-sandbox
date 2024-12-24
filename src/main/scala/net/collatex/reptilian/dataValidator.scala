d7b0204ed7cbcf0a08ebcc14bdcc877733ca52fe
rhdekker <ronald.dekker@di.huc.knaw.nl>
2024-12-24 19:13:11 +0100

    Made a start with data validator. Find the associated hyperedges.

--- src/main/scala/net/collatex/reptilian/dataValidator.scala
+++ src/main/scala/net/collatex/reptilian/dataValidator.scala
@@ -5,5 +5,9 @@ import net.collatex.util.Hypergraph
 def validateData(hypergraph: Hypergraph[EdgeLabel, TokenRange], matches: Iterable[FullDepthBlock]) =
   // convert each match into two TokenRanges
   val result = matches.map(toTokenRanges)
-  println(result)
+  // each match has two vectors with token ranges associated with it
+  // for each token range find the associated hyperedge in the graph
+  val result2 = result.map(_.map(e => findInstanceInHypergraph(hypergraph, e.start)))
+
+  println(result2)
   ???
