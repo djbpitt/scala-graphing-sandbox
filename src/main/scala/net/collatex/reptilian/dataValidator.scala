95151f91974e623b1c4217f766e68cb491b6f527
rhdekker <ronald.dekker@di.huc.knaw.nl>
2024-12-24 19:32:29 +0100

    Made a start with data validator. Extracted function to transform a single block.

--- src/main/scala/net/collatex/reptilian/dataValidator.scala
+++ src/main/scala/net/collatex/reptilian/dataValidator.scala
@@ -2,12 +2,23 @@ package net.collatex.reptilian
 
 import net.collatex.util.Hypergraph
 
-def validateData(hypergraph: Hypergraph[EdgeLabel, TokenRange], matches: Iterable[FullDepthBlock]) =
-  // convert each match into two TokenRanges
-  val result = matches.map(toTokenRanges)
+def convertMatch(hypergraph: Hypergraph[EdgeLabel, TokenRange], block: FullDepthBlock): Unit =
+  // convert match into two TokenRanges
+  val result = toTokenRanges(block)
+
   // each match has two vectors with token ranges associated with it
   // for each token range find the associated hyperedge in the graph
-  val result2 = result.map(_.map(e => findInstanceInHypergraph(hypergraph, e.start)))
+  val result2 = result.map(e => findInstanceInHypergraph(hypergraph, e.start))
+  // for each block we now have a vector of hyperedges
+  // now we split the hyperedges according to the token ranges of the matches.
 
   println(result2)
-  ???
+
+def validateData(hypergraph: Hypergraph[EdgeLabel, TokenRange], matches: Iterable[FullDepthBlock]): Unit =
+  // call a single function for each block.
+  // for each block we perform multiple steps
+  matches.foreach(convertMatch(hypergraph, _))
+
+
+
+
