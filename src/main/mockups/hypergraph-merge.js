5613497c070960e8b74b4eadac0861562db2e597
David J Birnbaum <djbpitt@gmail.com>
2025-05-09 12:31:13 -0400

    First slow movement implemented for hypergraph merge mockup

--- src/main/mockups/hypergraph-merge.js
+++ src/main/mockups/hypergraph-merge.js
@@ -10,18 +10,35 @@ function radioEventDispatch(e) {
   if (e.target.value == 'after') {
     performMerge()
   } else {
-    
     reset()
   };
 }
 function performMerge() {
   console.log('clicked after');
+  let ABlines = document.getElementById('ABline');
+  moveSlowly(ABlines, 35);
 }
 function reset() {
-  moveSlowly(1, 10);
+  /* Reset is instantaneous (unlike performMerge)
+   * The top-level <g> has a scale() that we need to retain;
+   * All other instances of @transform on <g> elements involve
+   *   movement, which we undo by stripping the @transform */
+  console.log('clicked before');
+  let gs = document.querySelectorAll('g g');
+  for (var i = 0, len = gs.length; i < len; i++) {
+    gs[i].setAttribute('transform', 'translate(0, 0)');
+  }
 }
 function moveSlowly(e, n) {
-  for (var i = 0; i < n; i++) {
-    setTimeout(console.log, 500 * i, 'Iteration no.', i)
+  function timeoutLoop(i) {
+    if (i < n) {
+      setTimeout(function () {
+        e.setAttribute('transform', 'translate(' + i + ',0)')
+        timeoutLoop(i + 1);
+      },
+      40);
+    }
   }
+  timeoutLoop(0);
+  // Start the loop  }
 }
