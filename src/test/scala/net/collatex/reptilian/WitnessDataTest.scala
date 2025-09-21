cad7172ae907f181162975b225b72de66cbc0abf
djb <djbpitt@gmail.com>
2025-09-21 12:35:34 -0400

    Complete all unit tests for colors and tokens (`t`, `n`, `w`, `g`, other) for `WitnessData` derived from JSON manifest

--- src/test/scala/net/collatex/reptilian/WitnessDataTest.scala
+++ src/test/scala/net/collatex/reptilian/WitnessDataTest.scala
@@ -3,7 +3,7 @@ package net.collatex.reptilian
 import net.collatex.reptilian.GtaBuilder.*
 import net.collatex.reptilian.TokenEnum.{Token, TokenSep}
 import org.scalatest.funsuite.AnyFunSuite
-import ujson.Num
+import ujson.{Num, Value}
 
 import scala.io.Source
 import scala.util.Using
@@ -166,7 +166,7 @@ class WitnessDataTest extends AnyFunSuite:
       assert(gotPairs == exp)
 
       // WitId constant within witness
-      val tokenWitIds = witnessData.tokens.collect { case t: TokenEnum.Token => t.w }.distinct
+      val tokenWitIds = witnessData.tokens.map(_.w).distinct
       assert(tokenWitIds == List(witnessOffset))
 
       // g increases by 1 with each successive token within a witness
@@ -328,6 +328,93 @@ class WitnessDataTest extends AnyFunSuite:
     assert(results.forall(_.color.isEmpty))
   }
 
+  /** checkJsonColors()
+    *
+    * Tests differ in presence or absence of color specifications
+    *
+    * Color must be specified on all witnesses or no witnesses (tested elsewhere)
+    *
+    * @param manifestFilename
+    *   Bare filename as string; manifests are all in the same `src/test/resources/manifests` directory
+    * @param expectedColors
+    *   Color values are either all Some(colorName) or all None
+    */
+  private def checkJsonColors(
+      manifestFilename: String,
+      expectedColors: List[Option[String]]
+  ): Unit = {
+    val manifestUrlOpt = Option(getClass.getResource(s"/manifests/$manifestFilename"))
+      .getOrElse(fail(s"Fixture not found on classpath: $manifestFilename"))
+    val manifestPath = os.Path(manifestUrlOpt.toURI)
+    val manifestSource = ManifestSource.Local(manifestPath)
+    val manifest = retrieveManifestJson(manifestSource).getOrElse(fail(s"Could not load $manifestFilename"))
+    val Right(results) = jsonToWitnessData(manifest, cfg): @unchecked
+
+    assert(results.map(_.siglum.value).toList == List("A", "B", "C")) // protect against accidental reordering
+    // No fonts to keep fonts and colors orthogonal
+    assert(results.forall(_.font.isEmpty))
+    assert(results.map(_.color).toList == expectedColors)
+  }
+
+  private def checkJsonTokensTN(
+      manifestFilename: String,
+      // expected per witness, in manifest order: List of tokens (t, n)
+      expectedPerWitness: List[List[(String, String)]]
+  ): Unit = {
+    val manifestUrlOpt = Option(getClass.getResource(s"/manifests/$manifestFilename"))
+      .getOrElse(fail(s"Fixture not found on classpath: $manifestFilename"))
+    val manifestPath = os.Path(manifestUrlOpt.toURI)
+    val manifestSource = ManifestSource.Local(manifestPath)
+    val manifest = retrieveManifestJson(manifestSource).getOrElse(fail(s"Could not load $manifestFilename"))
+    val Right(results) = jsonToWitnessData(manifest, cfg): @unchecked
+
+    // Protect against accidental witness reordering
+    assert(results.map(_.siglum.value).toList == List("A", "B", "C"))
+    assert(results.size == expectedPerWitness.size) // number of witnesses
+
+    results.zipWithIndex.foreach { case (witnessData, witnessOffset) =>
+      val exp = expectedPerWitness(witnessOffset)
+      val gotPairs: Seq[(String, String)] = witnessData.tokens.collect { case t: TokenEnum.Token => (t.t, t.n) }
+      assert(gotPairs == exp)
+
+      // WitId constant within witness
+      val tokenWitIds = witnessData.tokens.map(_.w).distinct
+      assert(tokenWitIds == List(witnessOffset))
+
+      // g increases by 1 with each successive token within a witness
+      // NB: No test across witnesses because this test suite is for WitnessData instances, and
+      //   changes across witnesses are not within a single instance
+      val gs = witnessData.tokens.map(_.g)
+      assert(
+        gs == Range(gs.head, gs.last + 1),
+        s"g must be strictly increasing for witness ${witnessData.siglum.value}"
+      )
+    }
+  }
+
+  private def checkJsonTokensExtra(
+      manifestFilename: String,
+      // expected per witness, in manifest order: List of tokens (t, n)
+      expectedPerWitness: List[List[Map[String, Value]]]
+  ): Unit = {
+    val manifestUrlOpt = Option(getClass.getResource(s"/manifests/$manifestFilename"))
+      .getOrElse(fail(s"Fixture not found on classpath: $manifestFilename"))
+    val manifestPath = os.Path(manifestUrlOpt.toURI)
+    val manifestSource = ManifestSource.Local(manifestPath)
+    val manifest = retrieveManifestJson(manifestSource).getOrElse(fail(s"Could not load $manifestFilename"))
+    val Right(results) = jsonToWitnessData(manifest, cfg): @unchecked
+
+    // Protect against accidental witness reordering
+    assert(results.map(_.siglum.value).toList == List("A", "B", "C"))
+    assert(results.size == expectedPerWitness.size) // number of witnesses
+
+    results.zipWithIndex.foreach { case (witnessData, witnessOffset) =>
+      val exp = expectedPerWitness(witnessOffset)
+      val gotExtras: Seq[Map[String, Value]] = witnessData.tokens.collect { case t: TokenEnum.Token => t.other }
+      assert(gotExtras == exp, s"Error in other property of token")
+    }
+  }
+
   // JSON manifest tests with and without root font
   test("jsonToWitnessData fonts: root font, some local") {
     checkJsonFonts(
@@ -365,54 +452,65 @@ class WitnessDataTest extends AnyFunSuite:
   }
 
   test("jsonToWitnessData colors: all present are preserved (fonts None)") {
-    val manifestFilename = "jsonWithColors.json"
-    val manifestPath = os.pwd / "src" / "test" / "resources" / "manifests" / manifestFilename
-    val manifestSource = ManifestSource.Local(manifestPath)
-    val json = retrieveManifestJson(manifestSource).getOrElse(fail(s"Count not load $manifestFilename"))
-    val expected = Right(
-      Vector(
-        WitnessData(
-          Siglum("A"),
-          Some("red"),
-          None,
-          Vector(
-            Token("Some ", "some", 0, 0, Map()),
-            Token("content ", "content", 0, 1, Map()),
-            Token("A", "a", 0, 2, Map())
-          )
-        ),
-        WitnessData(
-          Siglum("B"),
-          Some("blue"),
-          Some("WitnessBFont"),
-          Vector(
-            Token("Some ", "some", 1, 4, Map()),
-            Token("content ", "content", 1, 5, Map()),
-            Token("B", "b", 1, 6, Map())
-          )
-        ),
-        WitnessData(
-          Siglum("C"),
-          Some("green"),
-          None,
-          Vector(
-            Token("Some ", "some", 2, 8, Map()),
-            Token("content ", "content", 2, 9, Map("x-extra" -> 123)),
-            Token("C", "c", 2, 10, Map())
-          )
-        )
-      )
+    checkJsonColors(
+      manifestFilename = "jsonWithColors.json",
+      expectedColors = List(Some("red"), Some("blue"), Some("green"))
     )
-    val result = jsonToWitnessData(json, cfg)
-    assert(result == expected)
   }
 
-  ignore("jsonToWitnessData colors: none specified -> WitnessData.color = None") { ??? }
+  test("jsonToWitnessData colors: none specified -> WitnessData.color = None") {
+    checkJsonColors(
+      manifestFilename = "jsonNoRootNoFonts.json",
+      expectedColors = List(None, None, None)
+    )
+  }
 
-  ignore("jsonToWitnessData tokens: per-witness tokenization and normalization") { ??? }
+  test("jsonToWitnessData tokens: per-witness tokenization and normalization, content") {
+    checkJsonTokensTN(
+      manifestFilename = "jsonContent.json",
+      expectedPerWitness = List(
+        List(("Some ", "some"), ("content ", "content"), ("A", "a")),
+        List(("Some ", "some"), ("content ", "content"), ("B", "b")),
+        List(("Some ", "some"), ("content ", "content"), ("C", "c"))
+      )
+    )
+  }
+
+  test("jsonToWitnessData tokens: per-witness tokenization and normalization, tokens with n") {
+    checkJsonTokensTN(
+      manifestFilename = "jsonTokensWithN.json",
+      expectedPerWitness = List(
+        List(("Some ", "some"), ("tokens ", "tokens"), ("A", "a")),
+        List(("Some ", "some"), ("tokens ", "tokens"), ("B", "b")),
+        List(("Some ", "some"), ("tokens ", "tokens"), ("C", "c"))
+      )
+    )
+  }
+
+  test("jsonToWitnessData tokens: per-witness tokenization and normalization, tokens without n") {
+    checkJsonTokensTN(
+      manifestFilename = "jsonTokensWithoutN.json",
+      expectedPerWitness = List(
+        List(("Some ", "some"), ("tokens ", "tokens"), ("A", "a")),
+        List(("Some ", "some"), ("tokens ", "tokens"), ("B", "b")),
+        List(("Some ", "some"), ("tokens ", "tokens"), ("C", "c"))
+      )
+    )
+  }
+
+  test("jsonToWitnessData tokens: extra properties") {
+    checkJsonTokensExtra(
+      manifestFilename = "jsonTokensWithoutN.json",
+      expectedPerWitness = List(
+        List(Map.empty, Map.empty, Map.empty),
+        List(Map.empty, Map.empty, Map.empty),
+        List(Map.empty, Map("x-extra" -> 123), Map.empty)
+      )
+    )
+  }
 
   // JSON integration tests (build())
-  test("build() with json manifest using Seq[WitnessData]") {
+  ignore("build() with json manifest using Seq[WitnessData]") {
     val manifestFilename = "jsonWithColors.json"
     val manifestPath = os.pwd / "src" / "test" / "resources" / "manifests" / manifestFilename
     val manifestSource = ManifestSource.Local(manifestPath)
