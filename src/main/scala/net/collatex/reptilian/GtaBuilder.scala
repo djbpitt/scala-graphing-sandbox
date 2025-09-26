2386b35a5d29662673581b8831541718c55d4fd3
djb <djbpitt@gmail.com>
2025-09-26 17:20:19 -0400

    Revise `buildFromWitnessData()`` to return true `Either`; previously always returned only `Right()` without checking points of failure

--- src/main/scala/net/collatex/reptilian/GtaBuilder.scala
+++ src/main/scala/net/collatex/reptilian/GtaBuilder.scala
@@ -84,38 +84,40 @@ object GtaBuilder:
   private[reptilian] def buildFromWitnessData(
       wits: Seq[WitnessData],
       defaultColors: List[String]
-  ) =
+  ): Either[String, (Vector[TokenEnum], List[Siglum], List[String], List[Option[String]])] =
     final case class Acc(
         gta: Vector[TokenEnum],
         sigla: List[Siglum],
         colors: List[String],
         fonts: List[Option[String]]
     )
-    val init = Acc(Vector.empty, Nil, Nil, Nil)
-    val out = wits.zipWithIndex.foldLeft(init) { case (acc0, (witData, witIndex)) =>
-      // 1) Insert TokenSep BETWEEN witnesses (not before first)
-      val acc1 =
-        if witIndex == 0 then acc0
-        else {
-          val sepG = acc0.gta.last.g + 1
-          val sepId = s"sep$sepG"
-          val sep = TokenEnum.TokenSep(
-            sepId,
-            sepId,
-            /* w = */ acc0.gta.last.w,
-            /* g = */ sepG
-          )
-          acc0.copy(gta = acc0.gta :+ sep)
-        }
-      // 2) Siglum + color + font (color is witness.color or palette default)
-      val siglum = witData.siglum
-      val color = witData.color.getOrElse(defaultColors(witIndex % defaultColors.length))
-      val font = witData.font
-      val acc2 = acc1.copy(sigla = acc1.sigla :+ siglum, colors = acc1.colors :+ color, fonts = acc1.fonts :+ font)
-      // 3) Emit tokens with w/g
-      acc2.copy(gta = acc2.gta ++ witData.tokens)
-    }
-    Right(out.gta, out.sigla, out.colors, out.fonts)
+    if wits.isEmpty then Left("buildFromWitnessData() received unexpected empty sequence of WitnessData instances")
+    else
+      val init = Acc(Vector.empty, Nil, Nil, Nil)
+      val out = wits.zipWithIndex.foldLeft(init) { case (acc0, (witData, witIndex)) =>
+        // 1) Insert TokenSep BETWEEN witnesses (not before first)
+        val acc1 =
+          if witIndex == 0 then acc0
+          else {
+            val sepG = acc0.gta.last.g + 1
+            val sepId = s"sep$sepG"
+            val sep = TokenEnum.TokenSep(
+              sepId,
+              sepId,
+              /* w = */ acc0.gta.last.w,
+              /* g = */ sepG
+            )
+            acc0.copy(gta = acc0.gta :+ sep)
+          }
+        // 2) Siglum + color + font (color is witness.color or palette default)
+        val siglum = witData.siglum
+        val color = witData.color.getOrElse(defaultColors(witIndex % defaultColors.length))
+        val font = witData.font
+        val acc2 = acc1.copy(sigla = acc1.sigla :+ siglum, colors = acc1.colors :+ color, fonts = acc1.fonts :+ font)
+        // 3) Emit tokens with w/g
+        acc2.copy(gta = acc2.gta ++ witData.tokens)
+      }
+      Right(out.gta, out.sigla, out.colors, out.fonts)
 
   /** Convert incoming XML manifest to Seq[WitnessData]
     *
