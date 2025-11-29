8c6ccf0110e726b68b13f4f608486ac19be5c9ea
rhdekker <ronald.dekker@di.huc.knaw.nl>
2025-11-29 18:01:20 +0100

    Changed the implementation of DirectedGraph case class of EdgeLabelledDirectedGraph from Map(N, (Set(N, E), Set(N, E))) to (Map(N, Set(N, N)), Map((N, N), E)). This handles duplicated edges with different labels on them better, meaning consistently.

--- src/main/scala/net/collatex/util/EdgeLabelledDirectedGraph.scala
+++ src/main/scala/net/collatex/util/EdgeLabelledDirectedGraph.scala
@@ -32,7 +32,7 @@ enum EdgeLabelledDirectedGraph[N, E]:
   case EmptyGraph()
   case SingleNodeGraph(node: N)
   case LabelledEdge(source: N, label: E, target: N)
-  case DirectedGraph(adjacencyMap: Map[N, (Set[(N, E)], Set[(N, E)])])
+  case DirectedGraph(adjacencyMap: Map[N, (Set[N], Set[N])], labels: Map[(N,N), E])
 
   // NOTE: here I need two infix operators, to implement the 'a' |-- label --> 'b' construct
   // where |-- is left associative, does the actual operation
@@ -45,9 +45,10 @@ enum EdgeLabelledDirectedGraph[N, E]:
       case (_: EmptyGraph[N, E], other: EdgeLabelledDirectedGraph[N, E]) => other
       case (one: EdgeLabelledDirectedGraph[N, E], _: EmptyGraph[N, E]) => one
       case (one: SingleNodeGraph[N, E], other: SingleNodeGraph[N, E]) =>
-        val t1: (Set[(N, E)], Set[(N, E)]) = (Set(), Set((other.node, edgeLabel)))
-        val t2: (Set[(N, E)], Set[(N, E)]) = (Set((one.node, edgeLabel)), Set())
-        DirectedGraph(Map.apply(one.node -> t1, other.node -> t2))
+        val t1: (Set[N], Set[N]) = (Set(), Set(other.node))
+        val t2: (Set[N], Set[N]) = (Set(one.node), Set())
+        DirectedGraph(Map.apply(one.node -> t1, other.node -> t2), 
+          Map.apply((one.node, other.node) -> edgeLabel))
       case (_, _) =>
         // This is just to make the method compile while working on the implementation
         EdgeLabelledDirectedGraph.empty[N, E]
@@ -56,16 +57,16 @@ enum EdgeLabelledDirectedGraph[N, E]:
   @targetName("end arrow operator")
   def -->:(weight: E): (EdgeLabelledDirectedGraph[N, E], E) = (this, weight)
 
-  def toMap: Map[N, (Set[(N, E)], Set[(N, E)])] =
+  def toMap: (Map[N, (Set[N], Set[N])], Map[(N, N), E]) =
     this match
-      case _: EmptyGraph[N, E] => Map.empty
+      case _: EmptyGraph[N, E] => (Map.empty, Map.empty)
       case g: SingleNodeGraph[N, E] =>
-        Map.apply(g.node -> (Set.empty[(N, E)], Set.empty[(N, E)]))
+        (Map.apply(g.node -> (Set.empty[N], Set.empty[N])), Map.empty)
       case e: LabelledEdge[N, E] =>
-        val item1 = e.source -> (Set.empty[(N, E)], Set((e.target, e.label)))
-        val item2 = e.target -> (Set((e.source, e.label)), Set.empty[(N, E)])  
-        Map.apply(item1, item2)
-      case g: DirectedGraph[N, E] => g.adjacencyMap
+        val item1 = e.source -> (Set.empty[N], Set(e.target))
+        val item2 = e.target -> (Set(e.source), Set.empty[N])
+        (Map.apply(item1, item2), Map.apply((e.source, e.target) -> e.label))
+      case g: DirectedGraph[N, E] => (g.adjacencyMap, g.labels)
 
   @targetName("overlay")
   def +(other: EdgeLabelledDirectedGraph[N, E]): EdgeLabelledDirectedGraph[N, E] =
@@ -74,10 +75,10 @@ enum EdgeLabelledDirectedGraph[N, E]:
       case (one: EdgeLabelledDirectedGraph[N, E], _: EmptyGraph[N, E]) => one
       case (one: EdgeLabelledDirectedGraph[N, E], other: EdgeLabelledDirectedGraph[N, E]) =>
         // convert graphs into two maps so that we can merge the graphs
-        val m1 = one.toMap
-        val m2 = other.toMap
+        val (am1, lm1) = one.toMap
+        val (am2, lm2) = other.toMap
         // create a new graph with the entries combined
-        DirectedGraph(m1 |+| m2)
+        DirectedGraph(am1 |+| am2, lm1 ++ lm2)
 
   def incomingEdges(node: N): Set[(N, E, N)] =
     (this, node) match
@@ -88,7 +89,7 @@ enum EdgeLabelledDirectedGraph[N, E]:
         else Set.empty
       case (g: DirectedGraph[N, E], target) =>
         g.adjacencyMap(target)._1
-          .map((source, edge) => (source, edge, target))
+          .map(source => (source, g.labels(source, target), target))
   
   def outgoingEdges(node: N): Set[(N, E, N)] =
     (this, node) match
@@ -99,7 +100,7 @@ enum EdgeLabelledDirectedGraph[N, E]:
         else Set.empty
       case (g: DirectedGraph[N, E], source) =>
         g.adjacencyMap(source)._2
-          .map((target, edge) => (source, edge, target))
+          .map(target => (source, g.labels(source, target), target))
 
 
 //  def node_size: Int =
