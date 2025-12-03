284089d04a10a44c3dbb347fe29544dd206abbcf
rhdekker <ronald.dekker@di.huc.knaw.nl>
2025-12-03 15:12:35 +0100

    Graph: Updated test expectation.

--- src/test/scala/net/collatex/util/GraphTest.scala
+++ src/test/scala/net/collatex/util/GraphTest.scala
@@ -1,15 +1,16 @@
 package net.collatex.util
 
 import net.collatex.reptilian.NodeType
-import net.collatex.util.Graph.DirectedGraph
+import net.collatex.util.Graph.{DirectedEdge, DirectedGraph}
 import org.scalatest.funsuite.AnyFunSuite
 
 class GraphTest extends AnyFunSuite:
   test("create DAG"):
     val edges = Set(("173", "254"), ("173", "173"), ("192", "ends"), ("starts", "255"), ("192", "192"), ("255", "ends"))
-    val expected = DirectedGraph(Map("173" -> (Set(), Set("254")), "254" -> (Set("173"), Set())))
+    val expected = DirectedEdge("173", "254")
     val result = Graph.edge(edges.head._1, edges.head._2)
     assert(result == expected)
+
   test("topological sort, fully ordered"):
     val g: Graph[NodeType] = DirectedGraph(
       Map(
