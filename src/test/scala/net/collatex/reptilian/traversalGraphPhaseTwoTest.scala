2c3044a2abdae2913e0a9ff8f217a92cf5d97cb5
David J Birnbaum <djbpitt@gmail.com>
2025-12-06 16:33:59 -0500

    Create test (currently producing incorrect results) for traversal graph phase 2 with adjacent transposition

--- src/test/scala/net/collatex/reptilian/traversalGraphPhaseTwoTest.scala
+++ src/test/scala/net/collatex/reptilian/traversalGraphPhaseTwoTest.scala
@@ -43,4 +43,43 @@ class traversalGraphPhaseTwoTest extends AnyFunSuite:
     ) // Weight 1 unless target is end node, in which case weight 0
   }
 
-  ignore("Construct traversal graph with transposition")
+  test("Construct traversal graph with adjacent transposition") {
+    val GTa = Vector(
+      TokenEnum.Token("The ", "the", 0, 0, Map()),
+      TokenEnum.Token("red ", "red", 0, 1, Map()),
+      TokenEnum.Token("striped ", "striped", 0, 2, Map()),
+      TokenEnum.Token("cat", "cat", 0, 3, Map()),
+      TokenEnum.TokenSep("sep0", "sep0", 0, 4),
+      TokenEnum.Token("The ", "the", 1, 5, Map()),
+      TokenEnum.Token("striped ", "striped", 1, 6, Map()),
+      TokenEnum.Token("red ", "red", 1, 7),
+      TokenEnum.Token("cat", "cat", 1, 8)
+    )
+    val w0Tokens: Vector[TokenEnum.Token] = Vector(
+      TokenEnum.Token("The ", "the", 0, 0, Map()),
+      TokenEnum.Token("red ", "red", 0, 1, Map()),
+      TokenEnum.Token("striped ", "striped", 0, 2, Map()),
+      TokenEnum.Token("cat", "cat", 0, 3, Map())
+    )
+    val w1Tokens: Vector[TokenEnum.Token] = Vector(
+      TokenEnum.Token("The ", "the", 1, 5, Map()),
+      TokenEnum.Token("striped ", "striped", 1, 6, Map()),
+      TokenEnum.Token("red ", "red", 1, 7),
+      TokenEnum.Token("cat", "cat", 1, 8)
+    )
+    val w0AsHypergraph = createHypergraphFromSingleton(w0Tokens, GTa)
+    val w1AsHypergraph = createHypergraphFromSingleton(w1Tokens, GTa)
+    val matchesProperties = createMatches(w0AsHypergraph, w1AsHypergraph)
+    val tg: EdgeLabeledDirectedGraph[DecisionGraphStepPhase2Enum, TraversalEdgeProperties] = traversalGraphPhase2(
+      matchesProperties.matchesAsHg,
+      matchesProperties.matchesSortedHead.toList,
+      matchesProperties.matchesSortedLast.toList
+    )
+    val edgesAsSeq = tg.edges.toSeq.sortBy(e => e._1.pos1)
+    tg.edges.foreach(e =>
+      System.err.println(s"Source: ${e.source.pretty}")
+      System.err.println(s"Target: ${e.target.pretty}")
+      System.err.println(s"Label: ${e.label}")
+    )
+    assert(1 == 1)
+  }
